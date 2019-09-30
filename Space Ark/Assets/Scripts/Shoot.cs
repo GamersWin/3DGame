b80f687e7e0a493672613a5ddddc721fa8736729
GamersWin <GamersWin@protonmail.com>
2019-09-30 09:18:44 +0300

    + Shoot

---
+++ Space Ark/Assets/Scripts/Shoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public int damage = 2; //переменная наносимого урона
+    public float range = 50.0f; // переменная дальности луча
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //блокируем курсор мыши
+        Cursor.visible = false; //скрываем курсор мыши
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {//нажимаем на левую кнопку мыши
+            Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(point); //создание луча методом ScreenPointToRay().
+            if (Physics.Raycast(ray, out hit, range))
+            {
+                Debug.Log(hit.transform.name); // выводим имя объекта, в который попадает луч
+                Strength objectVase = hit.transform.GetComponent<Strength>();
+                if(objectVase != null)
+                {
+                    objectVase.Damage(damage);
+                }
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        float posX = cam.pixelWidth / 2;
+        float posY = cam.pixelHeight / 2;
+        GUI.Label(new Rect(posX, posY, 12, 12), "*"); //команда GUI.Label() отображает символ *
+    }
+}
---
+++ Space Ark/Assets/Scripts/Strength.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strength : MonoBehaviour
+{
+    public int health = 10; //значение прочности предмета
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int value)
+    {
+        health -= value;
+        if(health <= 0)
+        {// если прочность предмета меньше 0
+            Destroy(gameObject); // уничтожаем объект методом Destroy()
+        }
+    }
+}
