ca6c881d25f48d702d0520ae6e8d253803ec609d
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2019-10-15 09:46:02 +0300

    + AIEnemy

---
+++ Space Ark/Assets/Scripts/AnimDoorOpen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimDoorOpen : MonoBehaviour
+{
+
+    private Animator doorAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnim = gameObject.GetComponent<Animator>();
+    }
+
+
+    public void Open()
+    {
+        doorAnim.SetBool("ParOpen", true);
+    }
+    public void Close()
+    {
+        doorAnim.SetBool("ParOpen", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Space Ark/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public GameObject[] targets;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            foreach(GameObject target in targets)
+            {
+                target.SendMessage("Open");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            foreach (GameObject target in targets)
+            {
+                target.SendMessage("Close");
+            }
+        }
+    }
+}
